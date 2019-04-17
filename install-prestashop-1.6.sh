7c2fc329543736f6cf25683dc0c532fc2ea9fff9
Andres Hdez <andresfhdezs@gmail.com>
2019-04-17 09:08:01 -0500

    Version final docker: Nginx + PHP + MySql + PhpMyAdmin + Prestashop

--- install-prestashop-1.6.sh
+++ install-prestashop-1.6.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # Descarga PrestaShop
-wget https://www.prestashop.com/download/old/prestashop_1.6.1.13.zip
+wget https://download.prestashop.com/download/releases/prestashop_1.6.1.23.zip
 
 # Descomprime PrestaShop
-unzip prestashop_1.6.1.13.zip
+unzip prestashop_1.6.1.23.zip
 
 # Elimina el .zip 
-rm prestashop_1.6.1.13.zip Install_PrestaShop.html
+rm prestashop_1.6.1.23.zip Install_PrestaShop.html
 
 # Copia la carpeta de PrestaShop a la carpeta publica
-cp -r prestashop/ public/prestashop
+cp -r prestashop/ public/
 
 rm -r prestashop
 
